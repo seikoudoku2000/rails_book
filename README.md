58365b0934ceda10297f6120a2028b722b314a57
Yosuke Tomita <seikoudoku777@gmail.com>
2013-02-01 02:43:25 +0900

    chapter6

---
+++ README.md
@@ -0,0 +1,2 @@
+RailsによるアジャイルWebアプリケーション開発 第4版 の写経用  
+http://www.amazon.co.jp/dp/4274068668/
---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '925e183b0a618b2c2d7a6528c1aff7c6d7b717512b0e28ff8f3ddd30de29f968d9e6b5138d3c4e9d8d12b224d52de57e71e9d617f0b7bb3f658d17852d1729aa'
